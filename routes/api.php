2da2bc2ecfb37fcac05f0db9e067ef6b5de8c9e7
Ruhan Ahmad <37498569+ruhanahmad@users.noreply.github.com>
2025-08-06 18:06:27 +0500

    lol

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ use App\Http\Controllers\MessageController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//private
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
